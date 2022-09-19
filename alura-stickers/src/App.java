c45be43cca3ffb92bd8b83683a5da16ed3b56e52
CrisMai <crismai.dev@gmail.com>
2022-09-19 14:13:12 -0300

    Refatoração e orientação a objetos

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,51 +1,41 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
-        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-       // System.out.println(body);
+        //String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoIMDB();
+        
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
 
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
        
-        // extrair os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        System.out.println("Tamanho: " + listaDeFilmes.size());
-        System.out.println("Getindex 0: " + listaDeFilmes.get(0));
-        System.out.println("ListaDeFilmes: " + listaDeFilmes);
-
 
         // exibir e manipular os dados
+        
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         var geradora = new GeradoraDeFigurinhas();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 3; i++) {
 
-            Map<String, String> filme = listaDeFilmes.get(i);
-
-            String urlImagem = filme.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
-            String titulo = filme.get("title");
-
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = "saida/" + titulo + ".png";
-
+            Conteudo conteudo = conteudos.get(i);
+        
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
+            
             geradora.cria(inputStream, nomeArquivo);
-            System.out.println(titulo);
+            
+            System.out.println(conteudo.getTitulo());
             System.out.println();
-
-        }
+        }  
            
     }
 }
+
+
